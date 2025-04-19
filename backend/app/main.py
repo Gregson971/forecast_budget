5744f6a70dd24d33a48fb28630f3d88bdc9dd84a
Grégory Lama <gregory.lama@gmail.com>
2025-04-20 00:37:42 +0200

    feat(backend): Handle CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 """Module principal de l'application."""
 
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.infrastructure.db.database import Base, engine
 from app.interfaces.api.auth import auth_router
 from app.interfaces.api.user import user_router
@@ -13,6 +15,18 @@ app = FastAPI(
     title="Forecast budget API",
     description="API pour l'application de gestion de budget",
     version="1.0.0",
+)
+
+# Configuration CORS
+origins_allowed = os.getenv("ORIGINS_ALLOWED")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins_allowed,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
